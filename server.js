3849a8e75a4ae090c615c8a2b6f5b12f8311cfb6
ngocdat <huynhngocdat2002.td@gmail.com>
2023-02-05 00:59:20 +0700

    update server

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(cors());
 app.get("/api/campaigns", verifyToken, (req, res) => {
   res.json(database.campaigns);
 });
-
-app.listen(4001, () => console.log("Server started on port 4001"));
+const PORT = process.env.port || 4001;
+app.listen(PORT, () => console.log("Server started on port 4001"));
